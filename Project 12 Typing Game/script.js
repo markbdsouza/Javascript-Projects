e430a926f607bc7330ed5732580173aee7ece351
Mark B DSouza <mark.benjamin.dsouza@gmail.com>
2020-04-28 23:22:50 +0530

    final changes for speed typer

--- Project 12 Typing Game/script.js
+++ Project 12 Typing Game/script.js
@@ -1,3 +1,5 @@
+import { typerSettings } from './wordHandling';
+
 const difficultyBar = document.getElementById('settings');
 const settingBtn = document.getElementById('setting-btn');
 const word = document.getElementById('word');
@@ -8,55 +10,10 @@ const difficultySelect = document.getElementById('difficulty');
 const endContainer = document.querySelector('.end-game-container');
 const wordContainer = document.querySelector('.word-container');
 const gameTypeSelect = document.getElementById('gameType');
-
-const LIST_OF_PARAGRAPHS = [
-  // `Trying to make a wise, good choice when thinking about what kinds of careers might be best for you is a hard thing to do.`,
-  `Trying to make a wise, good choice when thinking about what kinds of careers might be best for you is a hard thing to do. Your future may very well depend on the ways you go about finding the best job openings for you in the world of work. Some people will feel that there is one and only one job in the world for them. Hard thinking and a lot of hard work will help them find the one job that is best for them. Jobs are there for those with skills and a good work ethic. Many new young artists in the upper New England states are famous around the world as leaders in new American art. These fine artists are very good in their chosen fields and are willing to share their many talents by teaching others. The students have had the chance to learn and use skills in oil painting, sketching with chalk, sculpting, and weaving. Learning to typewrite is a skill that will help all of us in our work today. The development of the computer will open doors for people with the keyboarding skills and will make typing a necessity. Managers, as well as secretaries, will need skill at the keyboard to input data and process words. Therefore, good keyboarding skills may be important to you.`,
-
-  // `The cognitive characteristics of paragraph comprehension items were studied by comparing models that`,
-  `The cognitive characteristics of paragraph comprehension items were studied by comparing models that deal with two general processing stages: text representation and response decision. The models that were compared included the prepositional structure of the text (Kintsch & van Dijk, 1978), various counts of surface structure variables and word frequency (Drum et al., 1981), a taxonomy of levels of text questions (Anderson, 1972), and some new models that combine features of these models. Calibrations from the linear logistic latent trait model allowed evaluation of the impact of the cognitive variables on item responses. The results indicate that successful prediction of item difficulty is obtained from models with wide representation of both text and decision processing. This suggests that items can be screened for processing difficulty prior to being administered to examinees. However, the results also have important implications for test validity in that the two processing stages involve two different ability dimensions.`,
-
-  // `The word euthanasia is of Greek origin and literally means "a good death." The American`,
-  `The word euthanasia is of Greek origin and literally means "a good death." The American Heritage Dictionary defines it as "the act of killing a person painlessly for reasons of mercy." Such killing can be done through active means, such as administering a lethal injection, or by passive means, such as withholding medical care or food and water. In recent years in the United States, there have been numerous cases of active euthanasia in the news. They usually involve the deliberate killing of ill or incapacitated persons by relatives or friends who plead that they can no longer bear to see their loved ones suffer. Although such killings are a crime, the perpetrators are often dealt with leniently by our legal system, and the media usually portrays them as compassionate heroes who take personal risks to save another from unbearable suffering.`,
-
-  // `Autism spectrum disorders (ASD) are a range of psychological conditions characterized by`,
-  `Autism spectrum disorders (ASD) are a range of psychological conditions characterized by abnormalities in social interaction, behavior, interests, and communication. The five forms of ASD include classical autism, Asperger syndrome, Pervasive Developmental Disorder, Rett syndrome, and Childhood Disintegrative Disorder. Although the number of reported cases of ASD has experienced a dramatic increase in the past 25 years, the majority of doctors agree that this increase is due to changes in diagnostic practices and advances in the understanding of psychiatric health. While there is no general consensus among medical professionals about the underlying causes of ASD, theories range from genetic inheritance to environmental factors. One of the most controversial theories to have emerged in recent times is the hypothesis that ASD could be caused by the MMR vaccine, which is an immunization against measles, mumps, and rubella that was first developed in the 1960’s.`,
-
-  `Nature writing is nonfiction or fiction prose or poetry about the natural environment. Nature writing encompasses a wide variety of works, ranging from those that place primary emphasis on natural history facts (such as field guides) to those in which philosophical interpretation predominate. It includes natural history essays, poetry, essays of solitude or escape, as well as travel and adventure writing.`,
-];
-
-const paragraphList = {
-  alphabetTyper: {
-    value: 'a b c d e f g h i j k l m n o p q r s t u v w x y z',
-    difficulty: { easy: 2, medium: 1, hard: 0 },
-    returnParagraph() {
-      return this.value;
-    },
-  },
-  paraTyper: {
-    value: LIST_OF_PARAGRAPHS,
-    difficulty: { easy: 5, medium: 3, hard: 1 },
-    returnParagraph() {
-      return this.value[Math.floor(Math.random() * this.value.length)];
-    },
-  },
-  wordTyper: {
-    value: LIST_OF_PARAGRAPHS,
-    returnInfiniteModeWords() {
-      return Array.from(new Set(this.value.join(' ').split(' '))).filter(
-        (word) => word.length > 3
-      );
-    },
-    pickRandomWord() {
-      const length = this.returnInfiniteModeWords().length;
-      return this.returnInfiniteModeWords()[Math.floor(Math.random() * length)];
-    },
-  },
-};
-
-let paragraph = paragraphList['paraTyper'].returnParagraph();
-let paragraphWords = paragraph.split(' ');
 const startingTime = 10;
+const defaultOptions = { easy: 3, medium: 2, hard: 1 };
+
+let paragraphWords;
 let countDownInterval;
 let wordPicked;
 let timeLeft;
@@ -64,9 +21,7 @@ let score;
 let difficulty;
 let gameType;
 let paraWordIndex;
-let prevWord, currentWord, nextWord;
-const defaultOptions = { easy: 3, medium: 2, hard: 1 };
-let options = { easy: 3, medium: 2, hard: 0 };
+let options;
 
 /* Time related functions */
 function addToTimeCounter() {
@@ -75,6 +30,17 @@ function addToTimeCounter() {
 }
 function updateTimerinDOM(time) {
   timeSpan.innerText = `${time}s`;
+}
+function updateBodyBasedOnTime(time) {
+  let color;
+  if (time >= 6 || time === 0) {
+    color = 'rgb(240, 165, 0)';
+  } else if (time >= 3) {
+    color = 'rgb(255, 115, 0)';
+  } else if (time > 0) {
+    color = 'rgb(248, 39, 22)';
+  }
+  document.body.style.background = color;
 }
 function countDown() {
   updateTimerinDOM(startingTime);
@@ -85,8 +51,9 @@ function countDown() {
     } else {
       clearInterval(countDownInterval);
       wordInput.removeEventListener('input', checkWord);
-      endGame();
+      endGame((isTimeOver = true));
     }
+    updateBodyBasedOnTime(timeLeft);
   }, 1000);
 }
 
@@ -95,12 +62,11 @@ function updateScoreinDOM() {
 }
 
 function loadWord() {
-  wordPicked = paragraphList['wordTyper'].pickRandomWord();
+  wordPicked = typerSettings['wordTyper'].pickRandomWord();
   wordContainer.innerHTML = `<h1 class="word">${wordPicked}</h1>`;
 }
 
 function checkWord(e) {
-  console.log(wordPicked);
   if (e.target.value === wordPicked) {
     score++;
     updateScoreinDOM();
@@ -122,18 +88,12 @@ function difficultyChanged() {
 
 function gameTypeChanged() {
   gameType = gameTypeSelect.value;
-  if (gameType === 'alphabetTyper') {
-    paragraph = paragraphList['alphabetTyper'].returnParagraph();
-    paragraphWords = paragraph.split(' ');
+  if (gameType === 'paraTyper') {
+    paragraphWords = typerSettings['paraTyper'].returnParagraphArray();
   }
   restartGame();
   clearInterval(countDownInterval);
 }
-
-settingBtn.addEventListener('click', () => {
-  difficultyBar.classList.toggle('visible');
-});
-
 function loadPara() {
   paraWordIndex = paraWordIndex + 1 || 0;
   if (paraWordIndex === 0) {
@@ -149,7 +109,7 @@ function loadPara() {
       wordsDisplayed[2] || ''
     } </h1>`;
   } else if (paraWordIndex === paragraphWords.length) {
-    endGame();
+    endGame((isTimeOver = false));
   }
 }
 
@@ -162,14 +122,14 @@ function init() {
   difficulty = difficultySelect.value;
   wordInput.disabled = false;
   wordContainer.innerHTML = '';
-
   console.log(gameType);
-  console.log(paragraphList[gameType].difficulty);
-  options = paragraphList[gameType].difficulty || defaultOptions;
+  options = typerSettings[gameType].difficulty || defaultOptions;
+  if (gameType !== 'wordTyper') {
+    const isHard = difficulty === 'hard';
+    paragraphWords = typerSettings[gameType].returnParagraphArray(isHard);
+  }
   wordInput.focus();
 }
-
-function loadAlphabets() {}
 
 function startGame() {
   console.log('+', paragraphWords);
@@ -192,19 +152,26 @@ function restartGame() {
   loadEndContainerToDOM((isEnd = false));
 }
 
-function endGame() {
+function endGame(isTimeOver = true) {
   wordInput.disabled = true;
   wordInput.value = '';
-  loadEndContainerToDOM(true, score);
+  loadEndContainerToDOM(true, score, isTimeOver);
 }
 
-function loadEndContainerToDOM(isEnd, score) {
+function loadEndContainerToDOM(isEnd, score, isOutOfTime) {
   if (isEnd) {
-    endContainer.innerHTML = ` <h3>Time ran out</h3> You have scored a score of ${score} points in ${difficulty} difficulty. Nice Job!
+    endContainer.innerHTML = ` <h3>${
+      isOutOfTime ? `Time ran out!` : `Game Over`
+    }</h3> You have scored a score of ${score} points in ${difficulty} difficulty. 
+    ${isOutOfTime ? `Try harder!` : `Nice Job!`} 
     <button onClick='startGame()'> Play Again </button>`;
   } else {
-    endContainer.innerHTML = ` <h3>Welcome</h3> Select your mode and click play!
-    <button onClick='startGame()'> Play  </button>`;
+    endContainer.innerHTML = ` <h3>Welcome to the Speed Typing game</h3> 
+    <div> There are 3 modes of gameplay with 3 difficulty levels. All modes have a countdown timer. 
+    Depending on the mode & difficulty chosen, typing the right word will add seconds to the clock. 
+    Let's see how quickly you can type!
+    </div>    
+    <button onClick='startGame()'> Play </button>`;
   }
   endContainer.classList.add('visible');
 }
@@ -212,3 +179,6 @@ function loadEndContainerToDOM(isEnd, score) {
 loadEndContainerToDOM((isEnd = false));
 difficultySelect.addEventListener('change', difficultyChanged);
 gameTypeSelect.addEventListener('change', gameTypeChanged);
+settingBtn.addEventListener('click', () => {
+  difficultyBar.classList.toggle('visible');
+});
